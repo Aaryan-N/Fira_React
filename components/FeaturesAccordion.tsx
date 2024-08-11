9e4609641cd521997448219cd18349763d55ed79
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-08-11 15:08:20 +1000

    Initial commit with a few new things added

---
+++ components/FeaturesAccordion.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
+const FeaturesAccordion = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default FeaturesAccordion;
